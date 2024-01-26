9428284f412a9fcffff05d93b3f473f027750ab8
Adeolu Amole <adeoluamole@gmail.com>
2024-01-26 04:40:23 +0100

    Coding with Next and TypeScript day 4

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { links, links2 } from "./lib/SideBarLinks";
 export default function Home() {
   return (
     <main className="w-100 h-screen flex flex-col md:flex-row w-full">
-      <section className="bg-[#F4F4F4] w-[16.6%] px-[15px] pt-[15px] overflow-y-auto">
+      <section className="bg-[#F4F4F4] w-full md:w-[16.6%] px-[15px] pt-[15px] overflow-y-auto">
         <Section title="popular tags" links={links} />
         <Link href="" className="text-[14px] capitalize leading-[28px]">browse all tags</Link>
 
