89d7e529b77c7f7a31aa93db41f0b15c9cdae857
Adeolu Amole <adeoluamole@gmail.com>
2024-02-05 21:22:32 +0100

    Fixing Mongoose error

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -1,4 +1,4 @@
-import { UserModel } from "../models/user";
+import UserModel from "../models/user";
 import { connectToDb } from "../utils/database";
 
 export const RegisterUser = async (userData) => {
--- app/models/user.js
+++ app/models/user.js
@@ -8,5 +8,6 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true, minlength: 6 },
 });
 
-export const UserModel = mongoose.models?.User || mongoose.model("User", userSchema);
+const UserModel = mongoose.models?.User || mongoose.model("User", userSchema);
 
+export default UserModel;
