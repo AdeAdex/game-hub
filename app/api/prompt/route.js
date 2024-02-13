df430007431d151a6d3e58f4ca494751ae24713a
Adeolu Amole <adeoluamole@gmail.com>
2024-02-13 19:41:38 +0100

    fixing login page server side error

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,37 +1,42 @@
 import { connectToDb } from "../../utils/database";
 import User from "../../models/user";
 import comparePassword from "../../utils/bcrypt";
+import { NextResponse } from "next/server";
 
-export const POST = async (req, res) => {
-  const { email, password } = req.body;
-  console.log(req.body);
+export const POST = async (request, res) => {
+  const { email, password } = request.body;
+  console.log(request.body);
   try {
     await connectToDb();
-    // const loginDetails = await request.body.json();
-    // console.log(loginDetails);
-
     const user = await User.findOne({
       $or: [{ email }, { userName: email }],
     });
 
     if (!user) {
       console.log("User not found");
-      return res.status(404).json({ message: "User not found" });
+      return NextResponse.json({}, { status: 404 });
     }
 
     const passwordMatch = await comparePassword(password, user.password);
 
     if (!passwordMatch) {
-      return res.status(401).json({ message: "Invalid email or password" });
+      return NextResponse.json(
+        { message: "Invalid email or password" },
+        { status: 401 }
+      );
     }
 
     console.log("User found:", user);
-    return res.status(200).json(user);
+    return NextResponse.json(user, { status: 200 });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ message: "Internal server error" });
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
   }
 };
 
-
-export { handler as GET, handler as POST };
+export const GET = async (request, response) => {
+  // Implement GET method logic here if needed
+};
