5027cb902baf4dd1b09e985af196e2b116be3e33
Adeolu Amole <adeoluamole@gmail.com>
2024-03-07 09:35:46 +0100

    created component for the profile dropdown to structure the code

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -12,6 +12,7 @@ import { useSession, signOut } from "next-auth/react";
 // import Image from "next/image";
 import { FaAngleDown } from "react-icons/fa6";
 import { useSelector } from "react-redux";
+import ProfileDropdown from "./ProfileDropdown";
 // import Cookies from "universal-cookie";
 
 interface AuthState {
@@ -78,29 +79,7 @@ const Navbar: React.FC = () => {
                   <FaAngleDown size={18} className="my-auto" />
                 </div>
                 {dropdown && (
-                  <div className="absolute top-10 right-0 bg-white z-20 w-[200px] flex flex-col gap-[5px] text-[14px] rounded-sm border border-2">
-                    <small className="bg-[#F4F4F4] uppercase px-4 nav-dropdown py-1">
-                      Explore
-                    </small>
-                    <div className="px-4 nav-dropdown">My library</div>
-                    <div className="px-4 nav-dropdown">Recommendation</div>
-                    <div className="px-4 nav-dropdown">Game Jams</div>
-                    <small className="bg-[#F4F4F4] uppercase px-4 nav-dropdown py-1">
-                      Create
-                    </small>
-                    <div className="px-4 nav-dropdown">Dashboard</div>
-                    <div className="px-4 nav-dropdown">Posts</div>
-                    <div className="px-4 nav-dropdown">Upload new project</div>
-                    <div className="px-4 nav-dropdown">Host game jam</div>
-                    <small className="bg-[#F4F4F4] uppercase px-4 nav-dropdown py-1">
-                      Account
-                    </small>
-                    <div className="px-4 nav-dropdown">View profile</div>
-                    <div className="px-4 nav-dropdown">Settings</div>
-                    <div className="px-4 nav-dropdown" onClick={handleLogout}>
-                      logout
-                    </div>
-                  </div>
+                  <ProfileDropdown handleClick={handleLogout}/>
                 )}
               </div>
             ) : (
