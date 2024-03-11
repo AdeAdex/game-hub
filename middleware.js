476eacc070d6f4a5f04ccaf45cd62ff3a32fdf42
Adeolu Amole <adeoluamole@gmail.com>
2024-03-11 03:52:30 +0100

    working on user sign out if form was used to login/9

--- middleware.js
+++ middleware.js
@@ -1,26 +1,33 @@
-
 import jwt from "jsonwebtoken";
 // import { getSession } from "next-auth/react"
 
 export async function middleware(request) {
-  const token = request.cookies.get('loginToken')?.value;
+  const token = request.cookies.get("loginToken")?.value;
   // const session = await getSession({ req: request });
 
   // console.log("session", session);
 
   // if (session) {
   //   console.log("session", session);
-    
+
   // } else {
   //   console.log("no session at all bro");
   // }
- 
+
   // Exclude the homepage ("/") from token validation
-  if (request.nextUrl.pathname === '/' || request.nextUrl.pathname === '/register' || request.nextUrl.pathname === '/username' ) {
+  if (
+    request.nextUrl.pathname === "/" ||
+    request.nextUrl.pathname === "/register" ||
+    request.nextUrl.pathname === "/username" ||
+    request.nextUrl.pathname === "/forgot-password"
+  ) {
     return;
   }
- 
-  if ((token/*  || session?.user */) && !request.nextUrl.pathname.startsWith('/dashboard')) {
+
+  if (
+    token /*  || session?.user */ &&
+    !request.nextUrl.pathname.startsWith("/dashboard")
+  ) {
     // Decode the token to check if it's expired
     try {
       const decodedToken = jwt.decode(token);
@@ -28,7 +35,7 @@ export async function middleware(request) {
 
       if (decodedToken.exp && decodedToken.exp < currentTimestamp) {
         console.log("Token has expired, redirect to login");
-        request.cookies.delete('loginToken');
+        request.cookies.delete("loginToken");
         // return Response.redirect(new URL('/login', request.url));
       }
 
@@ -36,39 +43,36 @@ export async function middleware(request) {
       return;
     } catch (error) {
       // If there's an error decoding the token, redirect to login
-      console.error('Error decoding token:', error);
-      return Response.redirect(new URL('/login', request.url));
+      console.error("Error decoding token:", error);
+      return Response.redirect(new URL("/login", request.url));
     }
   }
- 
-  if ((!token/*  || !session?.user */) && !request.nextUrl.pathname.startsWith('/login')) {
+
+  if (
+    !token /*  || !session?.user */ &&
+    !request.nextUrl.pathname.startsWith("/login")
+  ) {
     // No token found, redirect to login
-    return Response.redirect(new URL('/login', request.url));
+    return Response.redirect(new URL("/login", request.url));
   }
 }
 
-
-
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-}
-
-
-
-  
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
 
 // export function middleware(request) {
 //   const token = request.cookies.get('loginToken')?.value
- 
+
 //   if (token && !request.nextUrl.pathname.startsWith('/dashboard')) {
 //     return Response.redirect(new URL('/dashboard', request.url))
 //   }
- 
+
 //   if (!token && !request.nextUrl.pathname.startsWith('/login')) {
 //     return Response.redirect(new URL('/login', request.url))
 //   }
 // }
- 
+
 // export const config = {
 //   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 // }
