8943035a2a0478465a00951d8a1bd4a4fd1799a1
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-04-10 07:41:57 +0100

    Update SocialMediaLogin.tsx
    I just updated the SocialMediaLogin component to route user to dashboard after successful login

--- app/components/login/SocialMediaLogin.tsx
+++ app/components/login/SocialMediaLogin.tsx
@@ -58,7 +58,20 @@ const SocialMediaLogin = () => {
   // }, [session, router]);
 
   const handleSignIn = async (providerId: string) => {
-    signIn(providerId);
+    
+    try {
+      const result = await signIn(providerId);
+
+      if (result && !result.error) {
+        router.push("/dashboard");
+      } else {
+        const errorMessage = result?.error || "Error during login";
+        // enqueueSnackbar(errorMessage, { variant: "error" });
+      }
+    } catch (error: any) {
+      console.error("Error during login:", error.message);
+    } 
+
   };
 
   return (
