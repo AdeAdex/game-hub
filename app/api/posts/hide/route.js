d118c45a96853fbd81dfc45cb1e7e2e6e4bc291b
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-04-24 21:27:27 +0100

    Update route.js

--- app/api/posts/hide/route.js
+++ app/api/posts/hide/route.js
@@ -5,13 +5,21 @@ import Post from "../../../models/post";
 
 export const PUT = async (req, res) => {
   try {
-    
+    const body = await req.json();
+		const postId = body.postId; 
+
 
     await connectToDb();
 
+    // Check if the post exists
+    const post = await Post.findById({ _id: postId });
+    if (!post) {
+      return NextResponse.json({ success: false, message: "Post not found."}, { status: 200 });
+    }
+
     // Implement logic to hide the post with the given postId
     // For example, update a field in the post document to indicate it's hidden
-    await Post.findByIdAndUpdate(postId, { hidden: true });
+    await Post.findByIdAndUpdate({ _id: postId} , { hidden: true });
 
     return NextResponse.json({ success: true, message: "Post hidden successfully." }, { status: 200 });
   } catch (error) {
