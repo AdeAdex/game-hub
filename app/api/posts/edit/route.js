8d63c221c30e5f67a356094ef8e26e39c4287837
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-04-24 17:26:34 +0100

    Update route.js

---
+++ app/api/posts/edit/route.js
@@ -0,0 +1,23 @@
+// /api/posts/edit/[postId]/route.ts
+import { NextResponse } from "next/server";
+import { connectToDb } from "../../../utils/database";
+import Post from "../../../models/post";
+
+export const PUT = async (req, res) => {
+  try {
+    const postId = req.query.postId;
+    const { content } = await req.json();
+
+    await connectToDb();
+
+    // Implement logic to update the content of the post with the given postId
+    await Post.findByIdAndUpdate(postId, { content });
+
+    return NextResponse.json({ success: true, message: "Post edited successfully." }, { status: 200 });
+  } catch (error) {
+    console.error("Error editing post:", error.message);
+    return NextResponse.error(new Error("Failed to edit post"), {
+      status: 500,
+    });
+  }
+};
