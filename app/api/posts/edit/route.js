1eaf01ce7742a749bcdd4983e5778c050ba207b1
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-04-24 21:10:27 +0100

    Update route.js

--- app/api/posts/edit/route.js
+++ app/api/posts/edit/route.js
@@ -6,9 +6,15 @@ import Post from "../../../models/post";
 export const PUT = async (req, res) => {
   try {
     const body = await req.json();
-		const { postId, content, image} = body; 
+    const { postId, content, image} = body; 
 
     await connectToDb();
+
+   // Check if the post exists
+    const post = await Post.findById({ _id: postId });
+    if (!post) {
+      return NextResponse.json({ success: false, message: "Post not found."}, { status: 200 });
+    } 
 
     // Implement logic to update the content of the post with the given postId
     await Post.findByIdAndUpdate(postId, { content });
