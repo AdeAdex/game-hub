51a04224bbceba259bdc03d451097e5b1af4705d
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-04-25 16:19:45 +0100

    Update PostActionModal.tsx

--- app/components/userPage/PostActionModal.tsx
+++ app/components/userPage/PostActionModal.tsx
@@ -207,7 +207,7 @@ const PostActionModal: React.FC<PostActionModalProps> = ( { open,
         </Box>
       </Modal>
       <AlertDialogSlide loading={loading} handleCloseDialog={handleCloseDialog} openDialog={openDialog} selectedPost={selectedPost} handleAction={handleAction} />
-      <PostModal handleUpdate={handleUpdate} open={open} selectedPost={selectedPost} />
+      <PostModal open={openEditModal} handleUpdate={handleUpdate} selectedPost={selectedPost} />
     </div>
   );
 };
