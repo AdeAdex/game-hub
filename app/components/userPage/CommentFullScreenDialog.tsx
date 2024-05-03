df74c5506c22166fd49f3574c25dd6438578c28f
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-03 11:11:49 +0100

    Update CommentFullScreenDialog.tsx

--- app/components/userPage/CommentFullScreenDialog.tsx
+++ app/components/userPage/CommentFullScreenDialog.tsx
@@ -9,9 +9,9 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import CloseIcon from '@mui/icons-material/Close';
 import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
+import { IoClose } from "react-icons/io5";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
@@ -52,7 +52,7 @@ export default function CommentFullScreenDialog({ openCommentDialog, setOpenComm
               onClick={handleClose}
               aria-label="close"
             >
-              <CloseIcon />
+<IoClose /> 
             </IconButton>
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
               Sound
