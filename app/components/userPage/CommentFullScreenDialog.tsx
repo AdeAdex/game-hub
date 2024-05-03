5b368acb5e90dcf47dab6ea69a13a31076336855
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-03 13:01:25 +0100

    Update CommentFullScreenDialog.tsx

--- app/components/userPage/CommentFullScreenDialog.tsx
+++ app/components/userPage/CommentFullScreenDialog.tsx
@@ -1,6 +1,6 @@
 'use client' 
 
-import * as React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import ListItemText from '@mui/material/ListItemText';
