11c291f660a9fb4ca165f90212956d324c6861a9
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-04 16:51:04 +0100

    Update route.js

--- app/api/posts/comments/route.js
+++ app/api/posts/comments/route.js
@@ -1,7 +1,7 @@
 // app/api/posts/comments/route.js
 
 
-import { NextApiResponse, NextApiRequest, NextResponse } from 'next';
+import { NextResponse } from 'next';
 import { connectToDb } from '../../../utils/database';
 import Post from '../../../models/post';
 
@@ -49,3 +49,42 @@ export const POST = async (req, res ) => {
     return NextResponse.json({ message: 'Failed to create comment' }, { status: 500 });
   }
 };
+
+
+
+
+
+export const GET = async (req, res) => {
+  if (req.method !== 'GET') {
+    return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 });
+  }
+
+  try {
+    // Extract postId from query parameters
+    const postId = await req.json(); 
+
+    // Ensure postId is provided
+    if (!postId) {
+      return NextResponse.json({ message: 'PostId is required' }, { status: 400 });
+    }
+
+    await connectToDb();
+
+    // Find the post by postId
+    const post = await Post.findById(postId);
+
+    // Check if the post exists
+    if (!post) {
+      return NextResponse.json({ message: 'Post not found' }, { status: 404 });
+    }
+
+    // Return the comments of the post
+    const comments = post.comments;
+    return NextResponse.json({ comments }, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching comments:', error.message);
+    return NextResponse.json({ message: 'Failed to fetch comments' }, { status: 500 });
+  }
+};
+
+
