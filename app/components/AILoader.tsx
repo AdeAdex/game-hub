6652ba4717d76207b2109c18c2b9dfb7496ec5a1
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-05 13:45:45 +0100

    Create AILoader.tsx

---
+++ app/components/AILoader.tsx
@@ -0,0 +1,18 @@
+<div className="ai-loader">
+  <div className="modelViewPort">
+    <div className="eva">
+      <div className="head">
+        <div className="eyeChamber">
+          <div className="eye"></div>
+          <div className="eye"></div>
+        </div>
+      </div>
+      <div className="body">
+        <div className="hand"></div>
+        <div className="hand"></div>
+        <div className="scannerThing"></div>
+        <div className="scannerOrigin"></div>
+      </div>
+    </div>
+  </div>
+</div>
