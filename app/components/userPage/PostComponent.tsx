89b84913ec1b08d8280ec0ea3e5803302fe6ff92
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-08 05:19:01 +0100

    Update PostComponent.tsx

--- app/components/userPage/PostComponent.tsx
+++ app/components/userPage/PostComponent.tsx
@@ -206,13 +206,19 @@ const PostComponent: React.FC<PostProps & { loggedInUserId: string }> = ({
       <>You liked</>
     ) : (
       // Other user liked the post
-      <>1 liked</>
+      <div className="flex gap-2">
+        <AiFillLike className="my-auto text-blue-500 bg-blue-500 p-1 rounded-full " size={15} />  
+        <span>1 liked </span>
+        <div/>
     )
   ) : (
     // More than one person liked the post
     likedPosts.includes(post._id) ? (
       // User liked the post
-      <><AiFillLike className="mr-2 bg-blue-500 p-1 rounded-full " size={15} /> You and {post.likedBy.length - 1} others</>
+      <div className="flex gap-2">
+        <AiFillLike className="my-auto text-blue-500 bg-blue-500 p-1 rounded-full " size={15} /> 
+        <span>You and {post.likedBy.length - 1} others </span>
+      <div/>
     ) : (
       // Others liked the post
       <>{post.likedBy.length} liked</>
