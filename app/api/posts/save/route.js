43db9379057e42cce548d568ff6d0e31d311b811
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-14 09:36:03 +0100

    Update route.js

--- app/api/posts/save/route.js
+++ app/api/posts/save/route.js
@@ -9,7 +9,20 @@ export const POST = async (req, res) => {
 
     await connectToDb();
 
-    // Implement logic to save the post for the user
+    // Check if the post is already saved by the user
+    const user = await User.findById(userId);
+    if (!user) {
+      return NextResponse.error(new Error("User not found"), { status: 404 });
+    }
+
+    if (user.savedPosts.includes(postId)) {
+      return NextResponse.json(
+        { success: false, message: "Post already saved." },
+        { status: 400 }
+      );
+    }
+
+    // If not already saved, save the post for the user
     await User.findByIdAndUpdate(userId, { $addToSet: { savedPosts: postId } });
 
     return NextResponse.json(
