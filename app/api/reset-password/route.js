13f06160e71a61e906d02a91ea7a3b49e14c819e
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-20 08:35:14 +0100

    Update route.js

--- app/api/reset-password/route.js
+++ app/api/reset-password/route.js
@@ -2,8 +2,19 @@
 
 import { NextResponse } from "next/server";
 import User from "../../models/user";
+import Activity from "../../models/activity";  // Import Activity model
 import { connectToDb } from "../../utils/database";
 import { hashPassword, comparePassword } from "@/app/utils/bcrypt";
+
+// Function to log activity
+const logActivity = async (userId, type, description) => {
+  const activity = new Activity({
+    userId,
+    type,
+    description,
+  });
+  await activity.save();
+};
 
 export const POST = async (req, res) => {
   if (req.method !== "POST") {
@@ -25,11 +36,10 @@ export const POST = async (req, res) => {
     const user = await User.findOne({ resetPasswordToken: token });
 
     if (!user) {
-      console.log("Invalid token or user not found")
+      console.log("Invalid token or user not found");
       return NextResponse.json({ error: "Invalid token or user not found" }, { status: 404 });
     }
     
-
     // Check if the new password is the same as the existing password
     const isSamePassword = await comparePassword(password, user.password);
     if (isSamePassword) {
@@ -39,13 +49,15 @@ export const POST = async (req, res) => {
 
     const hashedPassword = await hashPassword(password);
 
-
     // Update the user's password and clear the reset password token
     user.password = hashedPassword;
     user.resetPasswordToken = null;
     await user.save();
 
-    console.log("Password reset successfully")
+    // Log the password change activity
+    await logActivity(user._id, 'password_change', 'User changed password');
+
+    console.log("Password reset successfully");
     return NextResponse.json({ message: "Password reset successfully" }, { status: 200 });
   } catch (error) {
     console.error("Error resetting password:", error);
