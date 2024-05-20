b5262c955b43782bd01b932d9668a4a3467a8638
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-20 10:26:28 +0100

    Update activity.js

--- app/models/activity.js
+++ app/models/activity.js
@@ -1,3 +1,5 @@
+// /app/models/activity
+
 import mongoose from "mongoose";
 
 const ActivitySchema = new mongoose.Schema({
@@ -5,6 +7,8 @@ const ActivitySchema = new mongoose.Schema({
   type: { type: String, enum: ['login', 'password_change', 'profile_update'], required: true },
   description: { type: String, required: true },
   date: { type: Date, default: Date.now },
+  device: { type: String, required: true }, // Add device field
+  location: { type: String, required: true } // Add location field
 });
 
 const Activity = mongoose.models.Activity || mongoose.model("Activity", ActivitySchema);
