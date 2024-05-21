ece002c0c7c865b2d1f6cb06776e038123ed2eda
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-21 12:22:55 +0100

    Update Form.tsx

--- app/components/login/Form.tsx
+++ app/components/login/Form.tsx
@@ -52,33 +52,31 @@ function MyApp() {
     }
   }, [status, router]);
 
-   useEffect(() => {
-   
-    const fetchLocation = async () => {
-      try {
-        const response = await fetch("https://ipapi.co/json/");
-        const data = await response.json();
-        // console.log("location", data);
-        const deviceLocation = data ? `${data.city} ${data.region} ${data.country_name}` : "Location permission denied";
-        setLocation(deviceLocation);
-      } catch (error) {
-        console.error("Error fetching location:", error);
-      }
-    };
+   const fetchLocation = useCallback(async () => {
+    try {
+      const response = await fetch("https://ipapi.co/json/");
+      const data = await response.json();
+      const deviceLocation = data ? `${data.city} ${data.region} ${data.country_name}` : "Location permission denied";
+      setLocation(deviceLocation);
+    } catch (error) {
+      console.error("Error fetching location:", error);
+    }
+  }, []);
 
-  const detectDevice = () => {
+  const detectDevice = useCallback(() => {
     const parser = new UAParser();
     const result = parser.getResult();
-    // console.log("device", result)
     const deviceInfo = result.device.vendor
       ? `${result.device.vendor} ${result.device.model}`
       : result.os.name || "Unknown Device";
     setDevice(deviceInfo);
-  };
+  }, []);
 
-       fetchLocation();
-      detectDevice();
-  }, [location, device]);
+  useEffect(() => {
+    fetchLocation();
+    detectDevice();
+  }, [fetchLocation, detectDevice]);
+
 
 
   // Function to sign in with credentials and include device and location in request payload
