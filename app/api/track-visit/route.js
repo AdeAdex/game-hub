6f84b2eac74fe86bdf830f5d0dd1fba211288fa1
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-23 08:22:40 +0100

    Update route.js

--- app/api/track-visit/route.js
+++ app/api/track-visit/route.js
@@ -1,1 +1,23 @@
 // app/api/track-visit/route.js
+
+import { NextResponse } from "next/server";
+import { connectToDb } from "../../utils/database";
+import Visitor from "../../models/visitors.js";
+
+export const POST = async (req, res) => {
+  try {
+    const { postId, userId, reactionType } = await req.json();
+    await connectToDb();
+    const newVisitors = await Visitor.create({ postId, userId, reactionType });
+    return NextResponse.json(newReaction, { status: 201 });
+  } catch (error) {
+    console.error("Error creating reaction:", error.message);
+    return NextResponse.error(new Error("Failed to create reaction"), {
+      status: 500,
+    });
+  }
+};
+
+export const GET = async (request, response) => {
+  // Implement GET method logic here if needed
+};
