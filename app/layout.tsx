b7a7cbbcc9be5451984828bdbeabfed2e51c0627
Adeolu Amole <adeoluamole@gmail.com>
2024-05-23 10:45:05 +0100

    coding and fixing errors/2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,45 +2,25 @@
 
 
 'use client'
-
+// RootLayout.tsx
 import React, { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation'; // Import Suspense from next/navigation
 import "./globals.css";
 import { Inter } from 'next/font/google';
 import CustomProvider from './components/Provider';
 import ReduxProviders from './redux/Provider';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import Head from './head';
+import { Suspense } from 'react'
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const pathname = usePathname();
-  const searchParams = useSearchParams() ?? new URLSearchParams();
 
   useEffect(() => {
     const handleRouteChange = () => {
-      const params = new URLSearchParams(searchParams.toString());
-      const utmSource = params.get('utm_source');
-      const utmMedium = params.get('utm_medium');
-      const utmCampaign = params.get('utm_campaign');
-      const referrer = document.referrer;
-
-      // Send this information to your backend
-      fetch('/api/track-visit', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          referrer,
-          utmSource,
-          utmMedium,
-          utmCampaign,
-          url: `${pathname}?${searchParams.toString()}`,
-          userAgent: navigator.userAgent,
-        }),
-      });
+      // Your route change logic here
     };
 
     handleRouteChange(); // Track the initial load
@@ -54,14 +34,19 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       // Unsubscribe from route changes when component unmounts
       window.removeEventListener('routeChangeComplete', handleComplete);
     };
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return (
     <html lang="en">
       <Head />
       <body className={inter.className}>
         <ReduxProviders>
-          <CustomProvider>{children}</CustomProvider>
+          <CustomProvider>
+            {/* Wrap the children with Suspense */}
+            <Suspense fallback={<div>Loading...</div>}>
+              {children}
+            </Suspense>
+          </CustomProvider>
         </ReduxProviders>
         <SpeedInsights />
       </body>
