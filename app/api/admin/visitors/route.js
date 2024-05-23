32650a5aff83e9a2fadb6749aadaefa610929ddc
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-23 21:48:23 +0100

    Update route.js

--- app/api/admin/visitors/route.js
+++ app/api/admin/visitors/route.js
@@ -7,10 +7,10 @@ import Visitor from '../../../models/visitors';
 export const GET = async () => {
   try {
     await connectToDb();
-    const visitors = await Visitor.find().sort({ date: -1 }); // Fetch all visitors sorted by date
+    const visitors = await Visitor.find().sort({ date: -1 });
     return NextResponse.json(visitors, { status: 200 });
   } catch (error) {
     console.error("Error fetching visitors:", error.message);
-    return NextResponse.error(new Error("Failed to fetch visitors"), { status: 500 });
+    return NextResponse.json({ error: "Failed to fetch visitors" }, { status: 500 });
   }
 };
