f525903c9d313cb0807de9171eee177e0600ec69
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-23 08:33:41 +0100

    Update route.js

--- app/api/track-visit/route.js
+++ app/api/track-visit/route.js
@@ -4,20 +4,28 @@ import { NextResponse } from "next/server";
 import { connectToDb } from "../../utils/database";
 import Visitor from "../../models/visitors.js";
 
-export const POST = async (req, res) => {
+export const POST = async (req) => {
   try {
-    const { postId, userId, reactionType } = await req.json();
+    const { referrer, utmSource, utmMedium, utmCampaign, url, userAgent } = await req.json();
+    
     await connectToDb();
-    const newVisitors = await Visitor.create({ postId, userId, reactionType });
-    return NextResponse.json(newReaction, { status: 201 });
-  } catch (error) {
-    console.error("Error creating reaction:", error.message);
-    return NextResponse.error(new Error("Failed to create reaction"), {
-      status: 500,
-    });
-  }
-};
 
-export const GET = async (request, response) => {
-  // Implement GET method logic here if needed
+    const visitorData = {
+      referrer,
+      utmSource,
+      utmMedium,
+      utmCampaign,
+      url,
+      userAgent,
+      ipAddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    };
+
+    const newVisitor = new Visitor(visitorData);
+    await newVisitor.save();
+
+    return NextResponse.json(newVisitor, { status: 201 });
+  } catch (error) {
+    console.error("Error tracking visit:", error.message);
+    return NextResponse.error(new Error("Failed to track visit"), { status: 500 });
+  }
 };
