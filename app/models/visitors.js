316efa926c435f13bd71082185d67433fef9076b
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-23 08:32:22 +0100

    Update visitors.js

--- app/models/visitors.js
+++ app/models/visitors.js
@@ -3,12 +3,16 @@
 import mongoose from 'mongoose';
 
 const visitorSchema = new mongoose.Schema({
-  ipAddress: { type: String, required: true },
+  referrer: { type: String },
+  utmSource: { type: String },
+  utmMedium: { type: String },
+  utmCampaign: { type: String },
+  url: { type: String, required: true },
   userAgent: { type: String, required: true },
-  source: { type: String, enum: ['social_media', 'google', 'direct', 'other'], default: 'direct' },
+  ipAddress: { type: String },
   date: { type: Date, default: Date.now },
 });
 
-const Visitor = mongoose.model.Visitor || mongoose.model('Visitor', visitorSchema);
+const Visitor = mongoose.models.Visitor || mongoose.model('Visitor', visitorSchema);
 
 export default Visitor;
