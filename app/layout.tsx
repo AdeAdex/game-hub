385db5562a0f66ff1cbcc460b933f70dfb401028
Adeolu Amole <adeoluamole@gmail.com>
2024-05-23 10:40:46 +0100

    coding and fixing errors/1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,64 +1,67 @@
-'use client';
+// RootLayout.tsx
 
-import { useEffect, Suspense } from 'react';
+
+'use client'
+
+import React, { useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
-import { Inter } from 'next/font/google';
-import Head from './head';
 import "./globals.css";
-import CustomProvider from "./components/Provider";
-import ReduxProviders from "./redux/Provider";
+import { Inter } from 'next/font/google';
+import CustomProvider from './components/Provider';
+import ReduxProviders from './redux/Provider';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Head from './head';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
+  const searchParams = useSearchParams() ?? new URLSearchParams();
 
   useEffect(() => {
     const handleRouteChange = () => {
-      if (searchParams) {
-        const params = new URLSearchParams(searchParams.toString());
-        const utmSource = params.get('utm_source');
-        const utmMedium = params.get('utm_medium');
-        const utmCampaign = params.get('utm_campaign');
-        const referrer = document.referrer;
+      const params = new URLSearchParams(searchParams.toString());
+      const utmSource = params.get('utm_source');
+      const utmMedium = params.get('utm_medium');
+      const utmCampaign = params.get('utm_campaign');
+      const referrer = document.referrer;
 
-        // Send this information to your backend
-        fetch('/api/track-visit', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            referrer,
-            utmSource,
-            utmMedium,
-            utmCampaign,
-            url: `${pathname}?${searchParams.toString()}`,
-            userAgent: navigator.userAgent,
-          }),
-        });
-      }
+      // Send this information to your backend
+      fetch('/api/track-visit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          referrer,
+          utmSource,
+          utmMedium,
+          utmCampaign,
+          url: `${pathname}?${searchParams.toString()}`,
+          userAgent: navigator.userAgent,
+        }),
+      });
     };
 
     handleRouteChange(); // Track the initial load
-  }, [pathname, searchParams]); // Trigger effect on pathname or searchParams change
+
+    const handleComplete = () => handleRouteChange();
+
+    // Subscribe to route changes
+    window.addEventListener('routeChangeComplete', handleComplete);
+
+    return () => {
+      // Unsubscribe from route changes when component unmounts
+      window.removeEventListener('routeChangeComplete', handleComplete);
+    };
+  }, [pathname, searchParams]);
 
   return (
     <html lang="en">
       <Head />
       <body className={inter.className}>
         <ReduxProviders>
-          <CustomProvider>
-            <Suspense fallback={<div>Loading...</div>}>
-              {children}
-            </Suspense>
-        </CustomProvider>
+          <CustomProvider>{children}</CustomProvider>
         </ReduxProviders>
         <SpeedInsights />
       </body>
