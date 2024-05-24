62f167afd9e182af7fdcf6d1a26b5c559963791c
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-24 19:34:11 +0100

    Create ThemeToggle.js

---
+++ app/components/ThemeToggle.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import ThemeContext from '../lib/ThemeContext';
+
+const ThemeToggle = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext);
+
+    return (
+        <button onClick={toggleTheme} className="theme-toggle-btn">
+            {theme === 'light' ? '🌞' : '🌜'}
+        </button>
+    );
+};
+
+export default ThemeToggle;
