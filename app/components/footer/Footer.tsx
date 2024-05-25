d180b73d1d6ec3c1ff2956b377f07a99b3f6d81c
Adeolu Amole <adeoluamole@gmail.com>
2024-05-25 11:15:18 +0100

    coding on the landing page/12

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,18 +1,11 @@
-import React, { useContext } from "react";
+import React from "react";
 import FooterRowOne from "./FooterRowOne";
 import FooterRowTwo from "./FooterRowTwo";
-import { ThemeContext } from "@/app/lib/ThemeContext"; // Import ThemeContext
 
 const Footer = () => {
-  const { theme } = useContext(ThemeContext); // Get the current theme
-
   return (
     <main>
-      <footer
-        className={`flex flex-col justify-center px-[10px] md:px-[30px] md:px-[unset] md:items-center py-[20px] pt-[30px] ${
-          theme === "dark" ? "text-white bg-gray-900" : "text-[#434343] bg-[#F4F4F4]"
-        }`}
-      >
+      <footer className="flex flex-col justify-center px-[10px] md:px-[30px] md:px-[unset] md:items-center py-[20px] pt-[30px] text-[#434343] bg-[#F4F4F4]">
         {/* Row One */}
         <FooterRowOne />
 
--- app/components/footer/FooterRowTwo.tsx
+++ app/components/footer/FooterRowTwo.tsx
@@ -1,13 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import Link from "next/link";
-import { ThemeContext } from "@/app/lib/ThemeContext"; // Import ThemeContext
 
 const FooterRowTwo = () => {
-  const { theme } = useContext(ThemeContext); // Get the current theme
-
   return (
     <>
-      <div className={`flex flex-col md:flex-row gap-[30px] py-[20px] text-[14px] ${theme === "dark" ? "text-white" : "text-gray-800"}`}>
+      <div className="flex flex-col md:flex-row gap-[30px] py-[20px] text-[14px]">
         <div>Copyright &copy; {new Date().getFullYear()} Adex.</div>
         <ul className="flex md:gap-[30px] pl-[20px] md:pl-[unset] justify-between capitalize list-disc">
           <Link href="">
