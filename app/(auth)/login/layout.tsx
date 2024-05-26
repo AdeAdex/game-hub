955f5a0af9eaaacc0b7316698f0b256205aa4369
Adeolu Amole <adeoluamole@gmail.com>
2024-05-26 06:39:03 +0100

    coding and fixing issues/1

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -1,25 +1,5 @@
-/*import Navbar from "@/app/components/login/Navbar";
-import Footer from "@/app/components/footer/Footer";
-import React, { ReactNode } from "react";
-
-interface LoginLayoutProps {
-  children: ReactNode;
-}
-
-const LoginLayout: React.FC<LoginLayoutProps> = ({ children }) => {
-  return (
-    <main className="w-full bg-[#F4F4F4] md:h-screen">
-      <Navbar />
-      {children}
-      <Footer/>
-    </main>
-  );
-};
-
-export default LoginLayout;
-*/
-
 "use client";
+
 import Navbar from "@/app/components/login/Navbar";
 import Footer from "@/app/components/footer/Footer";
 import React, { ReactNode } from "react";
--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,18 +1,21 @@
-/*import React from "react";
+"use client";
+
+import React, { useContext } from "react";
 import FooterRowOne from "./FooterRowOne";
 import FooterRowTwo from "./FooterRowTwo";
-import { ThemeContext } from "@/app/lib/ThemeContext"; // Import ThemeContext
+import { ThemeContext } from "@/app/lib/ThemeContext";
 
 const Footer = () => {
-  const { theme } = useContext(ThemeContext); // Get the current theme
+  const { theme } = useContext(ThemeContext);
 
   return (
     <main>
-      <footer className="flex flex-col justify-center px-[10px] md:px-[30px] md:px-[unset] md:items-center py-[20px] pt-[30px] text-[#434343] bg-[#F4F4F4]">
-        {/* Row One */}
+      <footer className={`flex flex-col justify-center px-[10px] md:px-[30px] md:px-[unset] md:items-center py-[20px] pt-[30px] ${theme === "dark" ? "text-white bg-gray-800" : "text-[#434343] bg-[#F4F4F4]"}`}>
+        
+        {/* Footer Row One */}
         <FooterRowOne />
 
-        {/* Row Two */}
+        {/* Footer Row Two */}
         <FooterRowTwo />
       </footer>
     </main>
