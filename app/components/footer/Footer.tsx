f2d9d4d6817404ec50c771c43450caf6ee2cb616
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-26 06:08:11 +0100

    Update Footer.tsx

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import FooterRowOne from "./FooterRowOne";
 import FooterRowTwo from "./FooterRowTwo";
 
@@ -6,6 +6,31 @@ const Footer = () => {
   return (
     <main>
       <footer className="flex flex-col justify-center px-[10px] md:px-[30px] md:px-[unset] md:items-center py-[20px] pt-[30px] text-[#434343] bg-[#F4F4F4]">
+      
+        <FooterRowOne />
+
+        
+        <FooterRowTwo />
+      </footer>
+    </main>
+  );
+};
+
+export default Footer;
+*/
+
+
+import React, { useContext } from "react";
+import FooterRowOne from "./FooterRowOne";
+import FooterRowTwo from "./FooterRowTwo";
+import { ThemeContext } from "@/app/lib/ThemeContext";
+
+const Footer = () => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <main>
+      <footer className={`flex flex-col justify-center px-[10px] md:px-[30px] md:px-[unset] md:items-center py-[20px] pt-[30px] ${theme === "dark" ? "text-white bg-gray-800" : "text-[#434343] bg-[#F4F4F4]"}`}>
         {/* Row One */}
         <FooterRowOne />
 
