c972a85bd53c0588ecf7e87962b95ffe0bb3a1d2
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-27 17:00:19 +0100

    Update CardSkeleton.tsx

--- app/components/homePage/CardSkeleton.tsx
+++ app/components/homePage/CardSkeleton.tsx
@@ -1,27 +1,5 @@
 // /app/components/CardSkeleton.tsx
 
-/*"use client";
-
-import React, { useContext } from "react";
-import { ThemeContext } from "@/app/lib/ThemeContext";
-
-const CardSkeleton: React.FC = () => {
-  const { theme } = useContext(ThemeContext);
-
-  return (
-    <div className={`w-full md:w-[30%] lg:w-[19%] h-[300px] ${theme === "dark" ? "bg-gray-800" : "bg-gray-100"} rounded-lg shadow-md transform hover:scale-105 transition-transform duration-300 ease-in-out mt-3`}>
-      <div className={`animate-pulse relative w-100 h-[70%] ${theme === "dark" ? "bg-gray-700" : ""}`} />
-      <div className="p-4">
-        <div className={`animate-pulse h-6 w-3/4 mb-2 ${theme === "dark" ? "bg-gray-600" : "bg-gray-200"} rounded`} />
-        <div className={`animate-pulse h-4 w-1/2 ${theme === "dark" ? "bg-gray-600" : "bg-gray-200"} rounded`} />
-      </div>
-    </div>
-  );
-};
-
-export default CardSkeleton;
-*/
-
 "use client";
 
 import React, { useContext } from "react";
