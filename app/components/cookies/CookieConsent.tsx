97c3b382594952da2918539d3f773cfe9f76e30e
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-27 20:10:44 +0100

    Update CookieConsent.tsx

--- app/components/cookies/CookieConsent.tsx
+++ app/components/cookies/CookieConsent.tsx
@@ -35,8 +35,8 @@ const CookieConsent: React.FC<CookieConsentProps> = ({ onConsent }) => {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white p-4 z-50">
         <CloseIcon onClick={closeBanner} />
-      <div className="container mx-auto relative">
-        <p className="text-sm md:text-base px-[8px] md:px-[0px]">
+      <div className="container mx-auto relative px-[8px] md:px-[0px]">
+        <p className="text-sm md:text-base">
           We use cookies to enhance your experience. By continuing to visit this site you agree to our use of cookies.
           <Link href="/privacy-policy" className="underline ml-2">
             Learn more
