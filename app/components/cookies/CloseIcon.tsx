17f19b1f94ccb5c71d1287f884dd7dc2968465d8
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-31 11:24:27 +0100

    Update CloseIcon.tsx

--- app/components/cookies/CloseIcon.tsx
+++ app/components/cookies/CloseIcon.tsx
@@ -1,5 +1,5 @@
 // components/cookies/CloseIcon.tsx
-import React from 'react';
+/*import React from 'react';
 import { IoMdClose } from 'react-icons/io';
 
 const CloseIcon: React.FC<{ onClick: () => void }> = ({ onClick }) => {
@@ -7,3 +7,19 @@ const CloseIcon: React.FC<{ onClick: () => void }> = ({ onClick }) => {
 };
 
 export default CloseIcon;
+*/
+
+// components/cookies/CloseIcon.tsx
+import React from 'react';
+import { IoMdClose } from 'react-icons/io';
+
+const CloseIcon: React.FC<{ onClick: () => void }> = ({ onClick }) => {
+  return (
+    <div className="absolute top-2 right-3 cursor-pointer z-50 p-3 bg-red-500">
+      <IoMdClose onClick={onClick} size={18} className="text-white" />
+    </div>
+  );
+};
+
+export default CloseIcon;
+ 
