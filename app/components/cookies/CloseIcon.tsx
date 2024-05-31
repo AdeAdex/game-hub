c8b903665f142388e5fcc59632b27dff6045ada2
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-05-31 12:46:02 +0100

    Update CloseIcon.tsx

--- app/components/cookies/CloseIcon.tsx
+++ app/components/cookies/CloseIcon.tsx
@@ -5,7 +5,7 @@ import { IoMdClose } from 'react-icons/io';
 
 const CloseIcon: React.FC<{ onClick: () => void }> = ({ onClick }) => {
   return (
-    <div className="absolute top-1 right-1 cursor-pointer z-50 p-2 rounded-lg bg-gray-100">
+    <div className="absolute top-[~2px] right-1 cursor-pointer z-50 p-2 rounded-lg">
       <IoMdClose onClick={onClick} size={18} className="text-white hover:text-red-500" />
     </div>
   );
