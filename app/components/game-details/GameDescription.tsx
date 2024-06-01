7783bb2984b228686086d0de35d12cfd1db3b8d7
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-06-01 10:41:32 +0100

    Update GameDescription.tsx

--- app/components/game-details/GameDescription.tsx
+++ app/components/game-details/GameDescription.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "@/app/lib/ThemeContext";
 
 interface GameDescriptionProps {
   name_original: string;
@@ -14,23 +15,27 @@ const GameDescription: React.FC<GameDescriptionProps> = ({
   released,
   tba,
   updated,
-}) => (
-  <>
-    <p className="text-lg sm:text-xl text-gray-200 font-semibold mt-4">
-      <strong className="text-white">Original Name:</strong> {name_original}
-    </p>
-    <p className="text-gray-400 border-b-2 pb-3">
-      <strong>Description:</strong> {description_raw}
-    </p>
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-3">
-      <p className="text-gray-400">
-        <strong>Released:</strong> {released} {tba && "(To be announced)"}
+}) => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <>
+      <p className={`text-lg sm:text-xl font-semibold mt-4 ${theme === "dark" ? "text-gray-200" : "text-gray-800"}`}>
+        <strong className={`${theme === "dark" ? "text-white" : "text-black"}`}>Original Name:</strong> {name_original}
       </p>
-      <p className="text-gray-400">
-        <strong>Last Updated:</strong> {new Date(updated).toLocaleString()}
+      <p className={`border-b-2 pb-3 ${theme === "dark" ? "text-gray-400" : "text-gray-600"}`}>
+        <strong>Description:</strong> {description_raw}
       </p>
-    </div>
-  </>
-);
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-3">
+        <p className={`${theme === "dark" ? "text-gray-400" : "text-gray-600"}`}>
+          <strong>Released:</strong> {released} {tba && "(To be announced)"}
+        </p>
+        <p className={`${theme === "dark" ? "text-gray-400" : "text-gray-600"}`}>
+          <strong>Last Updated:</strong> {new Date(updated).toLocaleString()}
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default GameDescription;
