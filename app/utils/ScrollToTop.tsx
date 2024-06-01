998a8a312be3acd74efe8332c4f1aaf4bcf70a0e
Adeolu Amole <adeoluamole@gmail.com>
2024-06-01 07:16:16 +0100

    coding and working on  game details page/7

---
+++ app/utils/ScrollToTop.tsx
@@ -0,0 +1,45 @@
+// /app/components/utils/ScrollToTop.tsx
+
+import React, { useState, useEffect } from 'react';
+import { FaArrowUp } from 'react-icons/fa';
+
+const ScrollToTop: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div className="fixed bottom-4 right-4">
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="p-2 rounded-full bg-red-500 text-white shadow-lg hover:bg-red-600 transition duration-300"
+        >
+          <FaArrowUp size={24} />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ScrollToTop;
