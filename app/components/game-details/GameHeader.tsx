7adffc41502d0a1ad3ad71cc483920fbaace7b5d
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-06-01 10:43:00 +0100

    Update GameHeader.tsx

--- app/components/game-details/GameHeader.tsx
+++ app/components/game-details/GameHeader.tsx
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import Image from "next/image";
+import { ThemeContext } from "@/app/lib/ThemeContext";
 
 interface GameHeaderProps {
   name: string;
   backgroundImage: string;
 }
 
-const GameHeader: React.FC<GameHeaderProps> = ({ name, backgroundImage }) => (
-  <>
-    <h1 className="text-3xl sm:text-4xl font-bold mb-6 text-center text-red-500">
-      {name}
-    </h1>
-    <div className="relative w-full h-64 sm:h-[24rem] mb-6">
-      <Image
-        src={backgroundImage}
-        alt={name}
-        layout="fill"
-        objectFit="cover"
-        className="rounded-lg"
-      />
-    </div>
-  </>
-);
+const GameHeader: React.FC<GameHeaderProps> = ({ name, backgroundImage }) => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <>
+      <h1 className={`text-3xl sm:text-4xl font-bold mb-6 text-center ${theme === "dark" ? "text-red-500" : "text-red-600"}`}>
+        {name}
+      </h1>
+      <div className="relative w-full h-64 sm:h-[24rem] mb-6">
+        <Image
+          src={backgroundImage}
+          alt={name}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-lg"
+        />
+      </div>
+    </>
+  );
+};
 
 export default GameHeader;
