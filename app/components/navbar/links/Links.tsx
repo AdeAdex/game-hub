97010a52b4fe4973e4214da51108054a701373a9
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-06-22 16:42:51 +0100

    Update Links.tsx

--- app/components/navbar/links/Links.tsx
+++ app/components/navbar/links/Links.tsx
@@ -10,14 +10,14 @@ const Links: React.FC<LinksProps> = ({ ClassName }) => {
   const { theme } = useContext(ThemeContext); // Use the ThemeContext
 
   return (
-    <div className={`${theme === "dark" ? 'text-white' : 'text-[#434343]' } ${ClassName} my-auto uppercase nav-links   text-[14px] font-[700] overflow-x-auto relative`}>
-      <ul className="flex gap-4 my-auto">
+    <div className={`${theme === "dark" ? 'text-white' : 'text-[#434343]' } ${ClassName} my-auto uppercase nav-links gap-4 my-auto  text-[14px] font-[700] overflow-x-auto relative`}>
+      {/* <ul className="flex gap-4 my-auto"> */} 
         {links.map((link, index) => (
           <Link key={index} href={link.pathname} className="w-auto link py-2">
             <li className="w-auto whitespace-nowrap overflow-hidden">{link.title}</li>
           </Link>
         ))}
-      </ul>
+      {/* </ul> */} 
     </div>
   );
 };
