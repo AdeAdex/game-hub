fc694313ffed276470442fdbb626a027d9b553fb
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-06-22 16:20:14 +0100

    Update Dropdown.tsx

--- app/components/navbar/links/Dropdown.tsx
+++ app/components/navbar/links/Dropdown.tsx
@@ -42,7 +42,7 @@ const Dropdown: React.FC<SideBarProps> = ({
         suggestions={suggestions}
 
       />
-      <Links ClassName={`flex flex-col md:hidden`} />
+      <Links ClassName={`flex flex-col`} />
       <Section title="popular tags" links={links} />
       <Link href="" className="text-[14px] capitalize leading-[28px]">
         browse all tags →
