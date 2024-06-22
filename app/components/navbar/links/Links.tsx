45ebc67886585de77dfe4bfcf8b2a44dc87e1170
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-06-22 15:38:59 +0100

    Update Links.tsx

--- app/components/navbar/links/Links.tsx
+++ app/components/navbar/links/Links.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 import React, { useContext } from "react";
 import { ThemeContext } from "@/app/lib/ThemeContext"; // Import the ThemeContext
 
-
-const Links = () => {
+interface LinksProps {
+  ClassName?: string;
+} 
+const Links: React.FC<LinksProps> = ({ ClassName }) => {
   const { theme } = useContext(ThemeContext); // Use the ThemeContext
 
   return (
-    <div className={`${theme === "dark" ? 'text-white' : 'text-[#434343]' } hidden md:flex my-auto uppercase nav-links   text-[14px] font-[700] overflow-x-auto relative`}>
+    <div className={`${theme === "dark" ? 'text-white' : 'text-[#434343]' } ${ClassName} my-auto uppercase nav-links   text-[14px] font-[700] overflow-x-auto relative`}>
       <ul className="flex gap-4 my-auto">
         {links.map((link, index) => (
           <Link key={index} href={link.pathname} className="w-auto link py-2">
