c3487d279ee296ef2f34715116b76440ac9d4ef9
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-06-23 07:17:40 +0100

    Update index.tsx

---
+++ app/types/news/index.tsx
@@ -0,0 +1,15 @@
+// /types/news.ts
+
+export interface NewsArticle {
+  source: {
+    id: string | null;
+    name: string;
+  };
+  author: string | null;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string | null;
+  publishedAt: string;
+  content: string;
+}
