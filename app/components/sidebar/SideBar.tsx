b5c84a00a97cb49e6b41f0a33c9474e3db93bcd8
Adeolu Amole <adeoluamole@gmail.com>
2024-08-14 15:05:36 +0100

    feat: i include games route to browse all tags button

--- app/components/sidebar/SideBar.tsx
+++ app/components/sidebar/SideBar.tsx
@@ -22,7 +22,7 @@ const SideBar: React.FC<SideBarProps> = ({ popularTags, browse}) => {
   return (
     <section className="hidden md:flex flex-col w-full md:w-[16.6%] px-[15px] pt-[15px] overflow-y-auto fixed h-full bg-[#F4F4F4] text-black dark:bg-gray-900 dark:text-white">
       <Section title="popular tags" tags={popularTags} />
-      <Link href="" className="text-[14px] capitalize leading-[28px]">
+      <Link href="/games" className="text-[14px] capitalize leading-[28px]">
         browse all tags →
       </Link>
       <div className="mt-[30px] flex flex-col gap-y-[30px]">
